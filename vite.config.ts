f4c236c5e628ba2749362892312feb8d12775968
Gu-Miao <1450206741@qq.com>
2022-11-08 16:28:00 +0800

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Sheep Fucking Sheep",
+        short_name: "SFS",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        theme_color: "#000",
+        background_color: "#fff",
+        icons: [
+          {
+            src: "logo.png",
+            type: "image/png",
+            sizes: "64x64",
+          },
+          {
+            src: "logo192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "logo512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 8888,
   },
-})
+});
